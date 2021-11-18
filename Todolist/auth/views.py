55769f69d40e43f4f44bb6da819f140d2f48cc43
Faruq <faruqabdulsalam@yahoo.com>
2021-11-18 12:37:44 +0100

    Add login error message to login view

--- Todolist/auth/views.py
+++ Todolist/auth/views.py
@@ -23,20 +23,21 @@ def register():
     
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
+    nologin = False
     if current_user.is_authenticated:
         return redirect(url_for('home.homepage'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data.lower()).first()
         if user is None or not user.check_password(form.password.data):
-            flash('Invalid username or password')
-            return redirect(url_for('auth.login'))
-        login_user(user, remember=form.remember_me.data)
-        next_page = request.args.get('next')
-        if not next_page or url_parse(next_page).netloc != '':
-            next_page = url_for('home.homepage')
-        return redirect(next_page)
-    return render_template('auth/login.html', title='Sign In', form=form)
+            nologin = True
+        else:
+            login_user(user, remember=form.remember_me.data)
+            next_page = request.args.get('next')
+            if not next_page or url_parse(next_page).netloc != '':
+                next_page = url_for('home.homepage')
+            return redirect(next_page)
+    return render_template('auth/login.html', title='Sign In', form=form, message=nologin)
 
 @auth.route('/logout')
 def logout():
