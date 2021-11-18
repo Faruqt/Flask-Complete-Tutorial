c29b615323b5520844772def3064c031038a69f7
Faruq <faruqabdulsalam@yahoo.com>
2021-11-18 14:04:26 +0100

    Change catefory input label

--- Todolist/task/forms.py
+++ Todolist/task/forms.py
@@ -5,7 +5,7 @@ from .models import Category
 
 class TaskForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
-    category = SelectField('Choose a category', coerce=int , validators=[DataRequired()])
+    category = SelectField('Category', coerce=int , validators=[DataRequired()])
     date = DateField('Date', format='%Y-%m-%d' , validators=[DataRequired()])
     time = TimeField('Time', format='%H:%M' , validators=[DataRequired()])
     submit = SubmitField('Add task')
